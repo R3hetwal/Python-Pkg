4e56e111c3536f16011bb5d27d8ad7bf9f1f2c29
R3hetwal <rqrakshya@gmail.com>
2022-11-08 20:28:48 +0545

    initial commit

---
+++ packagerb/__init__.py
@@ -0,0 +1,8 @@
+class Good:
+    def __init__():
+        print("Constructor formed")
+
+    def goodfun(self, number):
+        print("This is a function")
+        return number
+
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="packagerb",
+version="0.2",
+description="This is code with rb package",
+long_description="This is a very very long description",
+author="Rakshya",
+packages=['packagerb'],
+install_requires=[])
