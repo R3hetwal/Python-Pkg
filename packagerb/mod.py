af576a6a70f261b28404e670f42128a769e19678
Rakshya Bhetwal <37236068+R3hetwal@users.noreply.github.com>
2022-11-10 21:07:29 +0545

    Add files via upload

---
+++ packagerb/mod.py
@@ -0,0 +1,9 @@
+class test:
+    s = 'Nepal is a beautiful country. It is a small landlocked nation.'
+    a = [100, 200, 300]
+    d=["test","test4"]
+    def start(arg):
+        print(f'arg = {arg}')
+
+
+    
